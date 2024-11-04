74f5dde906295eaa81186701d08bd4a9e3971023
Nahyun <from.owol@gmail.com>
2024-11-04 22:24:46 +0900

    feat: SearchBar 컴포넌트 UI 구현

--- fe/src/pages/RoomPage/index.tsx
+++ fe/src/pages/RoomPage/index.tsx
@@ -1,9 +1,11 @@
+import SearchBar from '@/components/common/SearchBar';
 import RoomHeader from './RoomHeader/RoomHeader';
 
 const RoomPage = () => {
   return (
     <div>
       <RoomHeader />
+      <SearchBar />
     </div>
   );
 };
