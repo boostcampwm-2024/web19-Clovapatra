39230d758ed69f8c730c387021990d22b601577f
Nahyun <from.owol@gmail.com>
2024-11-05 21:44:42 +0900

    chore: devtools 사용, uuid로 id 생성

--- fe/src/store/useRoomStore.ts
+++ fe/src/store/useRoomStore.ts
@@ -1,21 +1,30 @@
 import { Room, RoomStore } from '@/types/room';
+import { v4 } from 'uuid';
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
-const useRoomStore = create<RoomStore>((set) => ({
-  rooms: [],
-  addRoom: (roomName: string, nickname: string) => {
-    const newRoom: Room = {
-      id: Math.random().toString(36).substr(2, 9),
-      name: roomName,
-      creator: nickname,
-      players: [nickname],
-      isGameStarted: false,
-    };
+const useRoomStore = create<RoomStore>()(
+  devtools(
+    (set) => ({
+      rooms: [],
+      addRoom: (roomName: string, nickname: string) => {
+        const newRoom: Room = {
+          id: v4(),
+          name: roomName,
+          creator: nickname,
+          players: [nickname],
+          isGameStarted: false,
+        };
 
-    set((state) => ({
-      rooms: [...state.rooms, newRoom],
-    }));
-  },
-}));
+        set((state) => ({
+          rooms: [...state.rooms, newRoom],
+        }));
+      },
+    }),
+    {
+      name: 'Room Store',
+    }
+  )
+);
 
 export default useRoomStore;
