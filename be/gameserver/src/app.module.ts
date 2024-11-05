65b881917a225c509f876c862963509d7fa77c13
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-05 18:05:24 +0900

    config: redis 설정

--- be/gameserver/src/app.module.ts
+++ be/gameserver/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from './redis/redis.module';
 import appConfig from './config/app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
       isGlobal: true,
       load: [appConfig],
     }),
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
