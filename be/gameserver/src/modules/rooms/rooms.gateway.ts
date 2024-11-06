9df0c5e7588a9a40ac15e33896c1465065c9a5ab
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-06 16:20:00 +0900

    feat: 사용자 입장 이벤트

--- be/gameserver/src/modules/rooms/rooms.gateway.ts
+++ be/gameserver/src/modules/rooms/rooms.gateway.ts
@@ -6,10 +6,11 @@ import {
   ConnectedSocket,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { RedisService } from '../../redis/redis.service';
 import { Logger } from '@nestjs/common';
 import { CreateRoomDto } from './dto/create-room.dto';
-import { RedisService } from '../../redis/redis.service';
 import { RoomDataDto } from './dto/room-data.dto';
+import { JoinRoomDto } from './dto/join-data.dto';
 import { v4 as uuidv4 } from 'uuid';
 
 @WebSocketGateway({ namespace: '/rooms' })
@@ -49,6 +50,51 @@ export class RoomsGateway {
     } catch (error) {
       this.logger.error(`Error creating room: ${error.message}`, error.stack);
       throw error;
+    }
+  }
+
+  @SubscribeMessage('joinRoom')
+  async handleJoinRoom(
+    @MessageBody() joinRoomDto: JoinRoomDto,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const { roomId, playerNickname } = joinRoomDto;
+    this.logger.log(`Join room requested: ${roomId} by ${playerNickname}`);
+
+    try {
+      const roomDataString = await this.redisService.get<string>(
+        `room:${roomId}`,
+      );
+      if (!roomDataString) {
+        this.logger.log(`Room not found: ${roomId}`);
+        client.emit('error', 'Room not found');
+        return;
+      }
+
+      const roomData: RoomDataDto = JSON.parse(roomDataString);
+
+      if (roomData.players.length >= 4) {
+        this.logger.log(`Room ${roomId} is full`);
+        client.emit('error', 'Room is full');
+        return;
+      }
+
+      if (roomData.players.includes(playerNickname)) {
+        this.logger.warn(`Nickname already taken: ${playerNickname}`);
+        client.emit('error', 'Nickname already taken in this room');
+        return;
+      }
+
+      roomData.players.push(playerNickname);
+      await this.redisService.set(`room:${roomId}`, JSON.stringify(roomData));
+
+      client.join(roomId);
+      this.logger.log(`User ${playerNickname} joined room ${roomId}`);
+
+      this.server.to(roomId).emit('updateUsers', roomData.players);
+    } catch (error) {
+      this.logger.error(`Error joining room: ${error.message}`, error.stack);
+      client.emit('error', 'Failed to join the room');
     }
   }
 }
