3357d86f505e10ad3606d87f922e2bae468a9b4d
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-06 16:31:11 +0900

    refactor: starter 모듈 삭제

--- be/gameserver/src/modules/rooms/rooms.websocket.controller.ts
+++ be/gameserver/src/modules/rooms/rooms.websocket.controller.ts
@@ -9,7 +9,7 @@ import { JoinRoomDto } from './dto/join-data.dto';
 export class RoomsWebSocketController {
   @Post('createRoom')
   @ApiOperation({
-    summary: 'Create Room (WebSocket Event)',
+    summary: '게임 방 생성',
     description:
       'ws://clovapatra.com/rooms 에서 "createRoom" 이벤트를 emit해 사용합니다. 성공적으로 게임방이 생성되면 "roomCreated" 이벤트를 수신해 RoomData를 받을 수 있습니다.',
   })
@@ -32,7 +32,7 @@ export class RoomsWebSocketController {
 
   @Post('joinRoom')
   @ApiOperation({
-    summary: 'Join Room (WebSocket Event)',
+    summary: '게임 방 입장',
     description:
       'ws://clovapatra.com/rooms 에서 "joinRoom" 이벤트를 emit해 사용합니다. 성공적으로 입장하면 방의 모든 사용자에게 "updateUsers" 이벤트를 통해 갱신된 사용자 목록을 제공합니다.',
   })
