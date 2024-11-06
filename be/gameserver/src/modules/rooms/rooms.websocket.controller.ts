1b2c9b202e502ea21797c2ecbdd385cf9005b051
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-06 16:28:26 +0900

    feat: joinRoom 이벤트 swagger 명세

--- be/gameserver/src/modules/rooms/rooms.websocket.controller.ts
+++ be/gameserver/src/modules/rooms/rooms.websocket.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { RoomDataDto } from './dto/room-data.dto';
+import { JoinRoomDto } from './dto/join-data.dto';
 
 @ApiTags('Rooms (WebSocket)')
 @Controller('ws')
@@ -27,5 +28,22 @@ export class RoomsWebSocketController {
       players: [createRoomDto.creatorNickname],
       status: 'waiting',
     };
+  }
+
+  @Post('joinRoom')
+  @ApiOperation({
+    summary: 'Join Room (WebSocket Event)',
+    description:
+      'ws://clovapatra.com/rooms 에서 "joinRoom" 이벤트를 emit해 사용합니다. 성공적으로 입장하면 방의 모든 사용자에게 "updateUsers" 이벤트를 통해 갱신된 사용자 목록을 제공합니다.',
+  })
+  @ApiBody({ type: JoinRoomDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Room joined successfully',
+    isArray: true,
+    type: String,
+  })
+  joinRoom(@Body() joinRoomDto: JoinRoomDto): string[] {
+    return ['example-creator-nickname', joinRoomDto.playerNickname];
   }
 }
