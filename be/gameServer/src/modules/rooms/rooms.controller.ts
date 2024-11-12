90ef8b963bdec0ddda358b16a7d68aecb33ffc08
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-12 23:58:02 +0900

    feat: 게임방 목록 조회 get -> sse

--- be/gameServer/src/modules/rooms/rooms.controller.ts
+++ be/gameServer/src/modules/rooms/rooms.controller.ts
@@ -1,36 +1,40 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Logger, Sse, MessageEvent } from '@nestjs/common';
 import { RedisService } from '../../redis/redis.service';
 import { RoomDataDto } from './dto/room-data.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Observable, Subject } from 'rxjs';
 
-@ApiTags('Rooms (REST)')
+@ApiTags('Rooms')
 @Controller('rooms')
 export class RoomController {
   private readonly logger = new Logger(RoomController.name);
+  private readonly roomUpdateSubject = new Subject<MessageEvent>();
 
-  constructor(private readonly redisService: RedisService) {}
+  constructor(private readonly redisService: RedisService) {
+    this.redisService.subscribeToChannel('roomUpdate', async (message) => {
+      this.logger.log(`게임방 업데이트 감지: ${message}`);
 
-  @Get()
+      const roomKeys = await this.redisService.keys('room:*');
+      const rooms = await Promise.all(
+        roomKeys.map(async (key) => {
+          const roomData = await this.redisService.get<string>(key);
+          return JSON.parse(roomData) as RoomDataDto;
+        }),
+      );
+      this.roomUpdateSubject.next({ data: rooms });
+    });
+  }
+
+  @Sse()
   @ApiOperation({
-    summary: '게임 방 목록 조회',
-    description: 'Redis에서 저장된 모든 게임 방 목록을 조회합니다.',
+    summary: '게임 방 목록 조회하는 SSE',
+    description: 'roomData가 변경되었을 시 변경된 room 배열을 전송합니다.',
   })
   @ApiResponse({
-    status: 200,
     description: '게임 방 목록이 성공적으로 반환됩니다.',
     type: [RoomDataDto],
   })
-  async getRooms(): Promise<RoomDataDto[]> {
-    const roomKeys = await this.redisService.keys('room:*');
-    this.logger.log('게임 방 목록 조회 시작');
-
-    const rooms = await Promise.all(
-      roomKeys.map(async (key) => {
-        const roomData = await this.redisService.get<string>(key);
-        return JSON.parse(roomData) as RoomDataDto;
-      }),
-    );
-    this.logger.log(`게임 방 목록 조회 완료, ${rooms.length}개 방 반환`);
-    return rooms;
+  getRoomUpdates(): Observable<MessageEvent> {
+    return this.roomUpdateSubject.asObservable();
   }
 }
