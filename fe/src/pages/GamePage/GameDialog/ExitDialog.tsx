3d5ed4a9768a4cd2bf0f2fe7a3aa54fe980c7e31
Nahyun <from.owol@gmail.com>
2024-11-13 21:15:10 +0900

    feat: ExitDialog 컴포넌트 생성

---
+++ fe/src/pages/GamePage/GameDialog/ExitDialog.tsx
@@ -0,0 +1,48 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { gameSocket } from '@/services/gameSocket';
+import { signalingSocket } from '@/services/signalingSocket';
+import useRoomStore from '@/stores/zustand/useRoomStore';
+import { RoomDialogProps } from '@/types/roomTypes';
+import { useNavigate } from 'react-router-dom';
+
+const ExitDialog = ({ open, onOpenChange }: RoomDialogProps) => {
+  const { setCurrentRoom } = useRoomStore();
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    gameSocket.disconnect();
+    signalingSocket.disconnect();
+
+    setCurrentRoom(null);
+
+    navigate('/');
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>방 나가기</AlertDialogTitle>
+          <AlertDialogDescription>
+            정말로 방을 나가시겠습니까?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>취소</AlertDialogCancel>
+          <AlertDialogAction onClick={handleExit}>확인</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ExitDialog;
