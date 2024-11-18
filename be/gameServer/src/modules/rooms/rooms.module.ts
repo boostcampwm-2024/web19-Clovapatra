3507e7d7ee3e5b6b87b026bdfcd31948873ded2a
student079 <102852555+student079@users.noreply.github.com>
2024-11-18 11:23:15 +0900

    [BE] 게임방 입장이 안되는 경우를 처리한다. (#99)
    * docs: swagger socket event 이해하기 좋게 정리
    
    * refactor: pipe 적용
    
    입력 유효성 검증 분리
    
    * feat: 예외 처리 filter로 분리
    
    * docs: swagger optional

--- be/gameServer/src/modules/rooms/rooms.module.ts
+++ be/gameServer/src/modules/rooms/rooms.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RedisModule } from 'src/redis/redis.module';
 import { RoomsGateway } from './rooms.gateway';
-import { RoomsWebSocketController } from './rooms.websocket.controller';
+import { RoomsWebSocketOnController } from './rooms.websocket.on.controller';
+import { RoomsWebSocketEmitController } from './rooms.websocket.emit.controller';
 import { RoomController } from './rooms.controller';
 
 @Module({
   imports: [RedisModule],
   providers: [RoomsGateway],
-  controllers: [RoomsWebSocketController, RoomController],
+  controllers: [
+    RoomsWebSocketOnController,
+    RoomsWebSocketEmitController,
+    RoomController,
+  ],
 })
 export class RoomsModule {}
