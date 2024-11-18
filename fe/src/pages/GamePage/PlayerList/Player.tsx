96bc81b9b6543ddb392fe4a4646534f82feeab83
Nahyun <from.owol@gmail.com>
2024-11-18 22:02:59 +0900

    feat: 방장 여부 확인하는 util 함수 구현 및 적용

--- fe/src/pages/GamePage/PlayerList/Player.tsx
+++ fe/src/pages/GamePage/PlayerList/Player.tsx
@@ -2,13 +2,18 @@ import { Card, CardContent } from '@/components/ui/card';
 import { FaCrown } from 'react-icons/fa6';
 import VolumeBar from './VolumeBar';
 import { PlayerProps } from '@/types/roomTypes';
+import { isHost } from '@/utils/playerUtils';
 
 const Player = ({ playerNickname, isReady }: PlayerProps) => {
   return (
     <Card className="h-full">
       <CardContent className="flex h-[4.7rem] items-center justify-between p-4">
         <div className="flex items-center gap-2">
-          <FaCrown className="text-yellow-500" />
+          {isHost(playerNickname) ? (
+            <FaCrown className="text-yellow-500" />
+          ) : (
+            ''
+          )}
           <span className="font-galmuri">{playerNickname}</span>
           {isReady && <span className="text-sm text-green-500">준비 완료</span>}
         </div>
