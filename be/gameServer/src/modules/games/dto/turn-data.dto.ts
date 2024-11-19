4ae8748e4a937019da84fe3d3dbc35f8368bc60a
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-19 16:31:26 +0900

    feat: startGame 이벤트
    turnChanged 이벤트 발행

---
+++ be/gameServer/src/modules/games/dto/turn-data.dto.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export enum GameMode {
+  PRONUNCIATION = 'PRONUNCIATION',
+  CLEOPATRA = 'CLEOPATRA',
+}
+
+export class TurnDataDto {
+  @ApiProperty({
+    example: '6f42377f-42ea-42cc-ac1a-b5d2b99d4ced',
+    type: String,
+    description: '게임 방 ID',
+  })
+  roomId: string;
+
+  @ApiProperty({
+    example: 'player1',
+    type: String,
+    description: '해당 단계를 수행하는 플레이어 닉네임',
+  })
+  playerNickname: string;
+
+  @ApiProperty({
+    example: GameMode.PRONUNCIATION,
+    type: String,
+    description: '해당 단계 게임 모드',
+  })
+  gameMode: GameMode;
+
+  @ApiProperty({
+    example: 10,
+    type: Number,
+    description: '해당 단계 게임 모드를 수행할 때의 제한시간',
+  })
+  timeLimit: number;
+
+  @ApiProperty({
+    example:
+      '도토리가 문을 도로록, 드르륵, 두루룩 열었는가? 드로록, 두루륵, 두르룩 열었는가.',
+    type: String,
+    description: '게임모드가 PRONUNCIATION일 때 가사',
+    required: false,
+  })
+  lyrics?: string;
+}
--- be/gameServer/src/modules/games/games.gateway.ts
+++ be/gameServer/src/modules/games/games.gateway.ts
@@ -1,11 +1,26 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { RedisService } from '../../redis/redis.service';
 import { Logger, UseFilters } from '@nestjs/common';
 import { WsExceptionsFilter } from '../../common/filters/ws-exceptions.filter';
+import { RoomDataDto } from '../rooms/dto/room-data.dto';
+import { GameDataDto } from './dto/game-data.dto';
+import { TurnDataDto } from './dto/turn-data.dto';
+import { ErrorResponse } from '../rooms/dto/error-response.dto';
+import { io, Socket as ClientSocket } from 'socket.io-client';
+import {
+  createTurnData,
+  selectCurrentPlayer,
+  checkPlayersReady,
+} from './games-utils';
 
 @WebSocketGateway({
-  namespace: '/games',
+  namespace: '/rooms',
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
@@ -19,5 +34,84 @@ export class GamesGateway {
   @WebSocketServer()
   server: Server;
 
-  constructor(private readonly redisService: RedisService) {}
+  private voiceProcessingSocket: ClientSocket;
+
+  constructor(private readonly redisService: RedisService) {
+    this.voiceProcessingSocket = io(`wss://voice-processing.clovapatra.com`);
+    this.voiceProcessingSocket.on('connect', () => {
+      this.logger.log('Successfully connected to the voice processing server');
+    });
+  }
+
+  @SubscribeMessage('startGame')
+  async handleStartGame(@ConnectedSocket() client: Socket) {
+    const { roomId, playerNickname } = client.data;
+    this.logger.log(`Game start requested for room: ${roomId}`);
+
+    try {
+      const roomDataString = await this.redisService.get<string>(
+        `room:${roomId}`,
+      );
+
+      if (!roomDataString) {
+        this.logger.warn(`Room not found: ${roomId}`);
+        client.emit('error', 'Room not found');
+        return;
+      }
+
+      const roomData: RoomDataDto = JSON.parse(roomDataString);
+
+      if (roomData.hostNickname !== playerNickname) {
+        this.logger.warn(
+          `User ${client.data.playerNickname} is not the host of room ${roomId}`,
+        );
+        client.emit('error', 'Only the host can start the game');
+        return;
+      }
+
+      const allReady = checkPlayersReady(roomData);
+      if (!allReady) {
+        this.logger.warn(`Not all players are ready in room: ${roomId}`);
+        client.emit('error', 'All players must be ready to start the game');
+        return;
+      }
+
+      roomData.status = 'progress';
+
+      await this.redisService.set(
+        `room:${roomId}`,
+        JSON.stringify(roomData),
+        'roomUpdate',
+      );
+
+      const alivePlayers = roomData.players.map(
+        (player) => player.playerNickname,
+      );
+
+      const gameData: GameDataDto = {
+        gameId: roomId,
+        alivePlayers,
+        currentTurn: 1,
+        currentPlayer: selectCurrentPlayer(alivePlayers),
+        previousPitch: 0,
+        previousPlayers: [],
+      };
+      await this.redisService.set(`game:${roomId}`, JSON.stringify(gameData));
+
+      const turnData: TurnDataDto = createTurnData(roomData, gameData);
+
+      this.server.to(roomId).emit('turnChanged', turnData);
+      this.voiceProcessingSocket.emit('turnChanged', turnData);
+
+      this.logger.log(`Game started successfully in room: ${roomId}`);
+    } catch (error) {
+      this.logger.error(
+        `Error starting game in room ${roomId}: ${error.message}`,
+      );
+      const errorResponse: ErrorResponse = {
+        message: 'Failed to start the game',
+      };
+      client.emit('error', errorResponse);
+    }
+  }
 }
