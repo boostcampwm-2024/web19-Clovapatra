e7f684120f3f8962d7adcaf1c6a765f985f186a7
Nahyun <from.owol@gmail.com>
2024-11-19 14:13:53 +0900

    feat: 마이크 권한 요청 custom hook 구현

---
+++ fe/src/hooks/useAudioPermission.ts
@@ -0,0 +1,27 @@
+import { MEDIA_CONSTRAINTS } from '@/constants/webRTC';
+import { useState } from 'react';
+
+export const useAudioPermission = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const requestPermission = async () => {
+    setIsLoading(true);
+    try {
+      const stream =
+        await navigator.mediaDevices.getUserMedia(MEDIA_CONSTRAINTS);
+      return stream;
+    } catch (error) {
+      if (error instanceof Error) {
+        if (error.name === 'NotAllowedError') {
+          throw new Error('마이크 권한이 필요합니다.');
+        }
+        throw new Error('마이크 연결에 실패했습니다.');
+      }
+      throw error;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { requestPermission, isLoading };
+};
