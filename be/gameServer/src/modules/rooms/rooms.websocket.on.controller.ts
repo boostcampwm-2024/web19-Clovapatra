211af6fad253c94148584742bd7587e515f9bb45
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-19 11:34:45 +0900

    docs: swagger 업데이트

--- be/gameServer/src/modules/rooms/rooms.websocket.on.controller.ts
+++ be/gameServer/src/modules/rooms/rooms.websocket.on.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
 import { CreateRoomDto } from './dto/create-room.dto';
-import { RoomDataDto } from './dto/room-data.dto';
 import { JoinRoomDto } from './dto/join-data.dto';
-import { PlayerDataDto } from '../players/dto/player-data.dto';
 
 @ApiTags('Rooms (WebSocket: 서버에서 수신하는 이벤트)')
 @Controller('rooms')
@@ -15,15 +13,9 @@ export class RoomsWebSocketOnController {
       'wss://clovapatra.com/rooms 에서 "createRoom" 이벤트를 emit해 사용합니다. 성공적으로 게임방이 생성되면 "roomCreated" 이벤트를 발행해 RoomData를 전달합니다.',
   })
   @ApiBody({ type: CreateRoomDto })
-  createRoom(@Body() createRoomDto: CreateRoomDto): RoomDataDto {
+  createRoom(@Body() createRoomDto: CreateRoomDto) {
     // This method does not execute any logic. It's for Swagger documentation only.
-    return {
-      roomId: 'example-room-id',
-      roomName: createRoomDto.roomName,
-      hostNickname: createRoomDto.hostNickname,
-      players: [{ playerNickname: createRoomDto.hostNickname, isReady: false }],
-      status: 'waiting',
-    };
+    return createRoomDto;
   }
 
   @Post('joinRoom')
@@ -33,11 +25,8 @@ export class RoomsWebSocketOnController {
       'wss://clovapatra.com/rooms 에서 "joinRoom" 이벤트를 emit해 사용합니다. 성공적으로 입장하면 입장한 방의 사용자들에게 "updateUsers" 이벤트를 통해 갱신된 사용자 목록을 제공합니다.',
   })
   @ApiBody({ type: JoinRoomDto })
-  joinRoom(@Body() joinRoomDto: JoinRoomDto): PlayerDataDto[] {
-    return [
-      { playerNickname: 'example-creator-nickname', isReady: false },
-      { playerNickname: joinRoomDto.playerNickname, isReady: false },
-    ];
+  joinRoom(@Body() joinRoomDto: JoinRoomDto) {
+    return joinRoomDto;
   }
 
   @Post('disconnect')
@@ -48,30 +37,24 @@ export class RoomsWebSocketOnController {
   })
   disconnect(): void {}
 
-  // @Post('kickPlayer')
-  // @ApiOperation({
-  //   summary: '플레이어 강퇴',
-  //   description:
-  //     'wss://clovapatra.com/rooms 에서 "kickPlayer" 이벤트를 emit해 사용합니다. 성공적으로 강퇴되면 모든 클라이언트에게 "updateUsers" 이벤트를 발행합니다.',
-  // })
-  // @ApiBody({ type: String })
-  // kickPlayer(@Body() playerNickname: string): PlayerDataDto[] {
-  //   return [
-  //     { playerNickname: 'example-creator-nickname', isReady: false },
-  //     { playerNickname: playerNickname, isReady: false },
-  //   ];
-  // }
   @Post('setReady')
   @ApiOperation({
     summary: '플레이어 준비 완료',
     description:
-      'wss://clovapatra.com/rooms 에서 "setReady" 이벤트를 emit해 사용합니다. 성공적으로 처리되면 모든 클라이언트에게 "updateUsers" 이벤트를 발행합니다.',
+      'wss://clovapatra.com/rooms 에서 "setReady" 이벤트를 emit해 사용합니다. 이미 준비 완료라면 준비대기로 변하고, 준비 대기라면 준비 완료 상태로 바뀝니다. 성공적으로 처리되면 모든 클라이언트에게 "updateUsers" 이벤트를 발행합니다.',
   })
-  @ApiBody({ type: String })
-  ready(@Body() playerNickname: string): PlayerDataDto[] {
-    return [
-      { playerNickname: 'example-creator-nickname', isReady: false },
-      { playerNickname: playerNickname, isReady: false },
-    ];
+  ready() {
+    return;
+  }
+
+  @Post('kickPlayer')
+  @ApiOperation({
+    summary: '플레이어 강퇴',
+    description:
+      'wss://clovapatra.com/rooms 에서 "kickPlayer" 이벤트를 emit해 사용합니다. 성공적으로 강퇴되면 해당 방의 클라이언트에게 "kicked", "updateUsers" 이벤트를 발행합니다.',
+  })
+  @ApiBody({ type: String, description: '강퇴할 사용자 playerNickname' })
+  kickPlayer(@Body() playerNickname: string) {
+    return playerNickname;
   }
 }
