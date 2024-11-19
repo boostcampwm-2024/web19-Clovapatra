cd8092d8dbb357a75835769f25d3c01f55695275
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-11-19 16:31:56 +0900

    docs: swagger 정리

--- be/gameServer/src/modules/games/games.websocket.emit.controller.ts
+++ be/gameServer/src/modules/games/games.websocket.emit.controller.ts
@@ -1,6 +1,20 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Post } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { TurnDataDto } from './dto/turn-data.dto';
 
-@ApiTags('Games (WebSocket: 서버에서 발행하는 이벤트)')
-@Controller('games')
-export class RoomsWebSocketEmitController {}
+@ApiTags('Rooms (WebSocket: 서버에서 발행하는 이벤트)')
+@Controller('rooms')
+export class GamesWebSocketEmitController {
+  @Post('turnChanged')
+  @ApiOperation({
+    summary: '현재 단계 수행할 정보',
+    description: 'turnData를 전달해 현재 단계 수행에 대한 정보를 전달합니다.',
+  })
+  @ApiResponse({
+    description: '현재 단계 정보',
+    type: TurnDataDto,
+  })
+  turnChanged() {
+    return;
+  }
+}
