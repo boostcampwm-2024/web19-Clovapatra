cee97be63c8a8d840399689557822a5bf3f13369
Nahyun <from.owol@gmail.com>
2024-11-28 14:07:22 +0900

    feat: 페이지네이션 기능 추가
    SSE, RoomList, RoomListPage 로직 변경 및 상수 추가

--- fe/src/pages/RoomListPage/index.tsx
+++ fe/src/pages/RoomListPage/index.tsx
@@ -1,13 +1,16 @@
 import SearchBar from '@/components/common/SearchBar';
 import RoomHeader from './RoomHeader/RoomHeader';
 import RoomList from './RoomList/RoomList';
-import { useRoomsSSE } from '@/hooks/useRoomsSSE';
 import { useEffect, useState } from 'react';
 import CustomAlertDialog from '@/components/common/CustomAlertDialog';
+import useRoomStore from '@/stores/zustand/useRoomStore';
+import { useRoomsSSE } from '@/hooks/useRoomsSSE';
 
 const RoomListPage = () => {
   const [showAlert, setShowAlert] = useState(false);
   const [kickedRoomName, setKickedRoomName] = useState('');
+  const { rooms } = useRoomStore();
+  const isEmpty = rooms.length === 0;
 
   useRoomsSSE();
 
@@ -22,10 +25,19 @@ const RoomListPage = () => {
   }, []);
 
   return (
-    <div>
-      <RoomHeader />
-      <SearchBar />
-      <RoomList />
+    <div className="min-h-screen flex flex-col pb-16 relative">
+      <div className="flex-1">
+        <RoomHeader />
+        <SearchBar />
+        {isEmpty ? (
+          <div className="h-[calc(100vh-220px)] flex items-center justify-center font-galmuri text-muted-foreground text-xl">
+            생성된 방이 없습니다.
+          </div>
+        ) : (
+          <RoomList />
+        )}
+      </div>
+
       <CustomAlertDialog
         open={showAlert}
         onOpenChange={setShowAlert}
